d548644a2260111ba3b34c46ff5047c20c6f7534
Leandro Rial <Leandro_Rial@outlook.com>
2021-08-31 15:52:32 -0300

    hooks with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useRef } from "react";
+import { useTodos, TodosProvider, useAddTodo, useRemoveTodo } from "./hooks/useTodos";
+import "./App.css";
+
+const Heading = ({ title }: { title: string }) => <h2>{title}</h2>;
+
+const Box: React.FunctionComponent = ({ children }) => (
+  <div
+    style={{
+      padding: "1rem",
+      fontWeight: "bold",
+    }}
+  >
+    {children}
+  </div>
+);
+
+// ++++++++ Interface ++++++++++++
+
+const Button: React.FunctionComponent<
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > & {
+    title?: string;
+  }
+> = ({ title, children, style, ...rest }) => (
+  <button
+    {...rest}
+    style={{
+      ...style,
+      backgroundColor: "red",
+      color: "white",
+      border: "none",
+      fontSize: "xx-large",
+    }}
+  >
+    {title ?? children}
+  </button>
+);
+
+function UL<T>({
+  items,
+  render,
+  itemClick,
+}: React.DetailedHTMLProps<
+React.HTMLAttributes<HTMLUListElement>,
+HTMLUListElement
+> & {
+  items: T[];
+  render: (item: T) => React.ReactNode;
+  itemClick: (item: T) => void
+}) {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li onClick={() => itemClick(item)} key={index}>{render(item)}</li>
+      ))}
+    </ul>
+  );
+}
 
 function App() {
+  const todos = useTodos();
+  const addTodo = useAddTodo();
+  const removeTodo = useRemoveTodo();
+
+  const newTodoRef = useRef<HTMLInputElement>(null);
+
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      addTodo(newTodoRef.current.value);
+      newTodoRef.current.value = "";
+    }
+  }, [addTodo]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title="Introduccion" />
+      <Box>hello There</Box>
+
+      <Heading title="Todos" />
+      <UL
+        itemClick={(item) => alert(item.id)}
+        items={ todos }
+        render={(todo) => (
+          <>
+            {todo.text}
+            <button onClick={() => removeTodo(todo.id)}>Remove</button>
+          </>
+        )}
+      />
+      <div>
+        <input type="text" ref={newTodoRef} />
+        <Button onClick={onAddTodo}>Add Todo</Button>
+      </div>
     </div>
   );
 }
 
-export default App;
+const JustShowTodos = () => {
+  const todos = useTodos();
+  return (
+    <UL
+      items={ todos }
+      itemClick={() => {}}
+      render={(todo) => <>{todo.text}</>}
+    />
+  )
+}
+
+const AppWrapper = () => (
+  <TodosProvider initialTodos={[
+    { id: 0, text: "Helouda useContext", done: false },
+  ]}>
+    <div style={{
+      display: "grid",
+      gridTemplateColumns: "50% 50%"
+    }}>
+      <App></App>
+      <JustShowTodos />
+    </div>
+  </TodosProvider>
+)
+
+export default AppWrapper;
